f98e1146dbe46f5e2cb7d354a4b8d802b217a82a
Flavia Costa <flavinha.goncalves@gmail.com>
2023-05-28 22:21:21 -0300

    Ajuste app.component.scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ import { HttpClient } from '@angular/common/http';
         }
     `,
   ],
-  styleUrls: ['app.component.scss'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   productDialog: boolean;
